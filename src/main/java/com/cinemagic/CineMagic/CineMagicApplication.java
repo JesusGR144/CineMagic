75790911851112a51240d27a68139e12f3ad02fc
Jesús Gaytán Ramírez <jesusgaytanramirez144@gmail.com>
2024-07-22 10:44:10 -0600

    Creación del proyecto CineMagic

---
+++ src/main/java/com/cinemagic/CineMagic/CineMagicApplication.java
@@ -0,0 +1,13 @@
+package com.cinemagic.CineMagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMagicApplication.class, args);
+	}
+
+}
