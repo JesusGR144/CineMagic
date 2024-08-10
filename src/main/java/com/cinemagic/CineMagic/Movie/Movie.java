caec9809bb2e9cd0116d91260ec6f5cdb7ad2488
Jesús Gaytán Ramírez <jesusgaytanramirez144@gmail.com>
2024-08-10 01:45:57 -0600

    Implementación de Clases para Movie

--- src/main/java/com/cinemagic/CineMagic/Movie/Movie.java
+++ src/main/java/com/cinemagic/CineMagic/Movie/Movie.java
@@ -1,11 +1,18 @@
 package com.cinemagic.cinemagic.movie;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import com.cinemagic.cinemagic.raiting.Raiting;
+import com.cinemagic.cinemagic.schedule.Schedule;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,9 +35,14 @@ public class Movie {
     private Long id;
 
     private String title;
-    private String description;
+    // private String description;
     private String releaseDate;
-    private int duration;
+
+    @OneToMany(mappedBy = "movie")
+    private Set<Schedule> schedubles = new HashSet<>();
+
+    @OneToMany(mappedBy = "movie")
+    private Set<Raiting> raitings = new HashSet<>();
 
     @Enumerated(EnumType.STRING)
     private Genre genre;
--- src/main/java/com/cinemagic/CineMagic/Movie/MovieService.java
+++ src/main/java/com/cinemagic/CineMagic/Movie/MovieService.java
@@ -3,6 +3,8 @@ package com.cinemagic.cinemagic.movie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cinemagic.cinemagic.exceptions.ApiRequestException;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -27,12 +29,12 @@ public class MovieService {
     public Movie updateMovie(Long id, Movie movieDetails) {
         return movieRepository.findById(id).map(movie -> {
             movie.setTitle(movieDetails.getTitle());
-            movie.setDescription(movieDetails.getDescription());
+            // movie.setDescription(movieDetails.getDescription());
             movie.setReleaseDate(movieDetails.getReleaseDate());
-            movie.setDuration(movieDetails.getDuration());
+            // movie.setDuration(movieDetails.getDuration());
             movie.setGenre(movieDetails.getGenre());
             return movieRepository.save(movie);
-        }).orElseThrow(() -> new RuntimeException("Movie not found with id " + id));
+        }).orElseThrow(() -> new ApiRequestException("Movie not found with id " + id));
     }
 
     public void deleteMovie(Long id) {
